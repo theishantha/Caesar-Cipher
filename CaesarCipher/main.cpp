f6ae7a589a532c8e105c47f261eddd4ac83d9ad3
Ishantha Udara <ishanthaudara96@gmail.com>
2018-07-09 22:51:48 +0530

    updated

--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -10,7 +10,7 @@ int main(){
      cin >> choice;
      cin.ignore();
 
-        if(choice == 1){
+        if(choice == 1) {
             //encryption
             string msg;
             cout << "Message can only be alphabetic" << endl;
@@ -24,13 +24,17 @@ int main(){
 
             string encryptedText = msg;
 
-            for (int i=0; i< msg.size(); i++ ){
-                if (msg[i]==32){
+            for (int i=0; i< msg.size(); i++ ) {
+                if (msg[i]==32) {
                     continue; // 32 is ASCII of space character, we will ignore it
                 } else{
                     if(msg[i]+key) > 122 {
                         //after lowercase z move back to a, z's ASCII is 122
-                        int temp = ()
+                        int temp = (msg[i] + key) - 122;
+                        encryptedText[i] = 96 + temp;
+                    } else if(msg[i] + key > 90 && msg[i] <= 96 ){
+                        int temp = (msg[i] +key) -90;
+                        encryptedText[i]
                     }
                 }
             }
