0497e106b96febe6451093b4af4ab75ca136786e
Ishantha Udara <ishanthaudara96@gmail.com>
2018-07-09 22:07:25 +0530

    updated

--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(){
+
+     int choice;
+     cout << "CLI interface for Basic Crptographic method - Ceasar Cipher " << endl;
+     cout << "1. Encryption" << endl << "2. Decryption" << endl << "Enter choice(1,2): ";
+     cin >> choice;
+     cin.ignore();
+
+        if(choice == 1){
+            //encryption
+            string msg;
+            cout << "Message can only be alphabetic" << endl;
+            cout << "Enter message: ";
+
+         }
+
 }
+
+
+
